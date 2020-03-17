ef0668892d85d31b820c7c2ee7caf905ec830a2c
nanogrunge <gdg007@gmail.com>
2020-03-17 18:35:09 -0300

    Updates

---
+++ precache-manifest.a6c435d78f194aa01d1ad7b6e549fde7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf1d70658f539028a14499c4477ea5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad4e00cb16058f02d2f9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d8e5d610d7b94d70a06",
+    "url": "/static/js/2.a37c7583.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a37c7583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4e00cb16058f02d2f9",
+    "url": "/static/js/main.63a6f9b2.chunk.js"
+  },
+  {
+    "revision": "30967c40040320d0b91a",
+    "url": "/static/js/runtime-main.021e82a6.js"
+  }
+]);
